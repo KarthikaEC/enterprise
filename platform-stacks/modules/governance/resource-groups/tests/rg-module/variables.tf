e71332674e1c8ddb9df51605affd666747016ebe
KarthikaEC <karthika@Elastacloud.onmicrosoft.com>
2026-07-26 11:57:31 +0530

    removed some vars

--- platform-stacks/modules/governance/resource-groups/tests/rg-module/variables.tf
+++ platform-stacks/modules/governance/resource-groups/tests/rg-module/variables.tf
@@ -2,21 +2,6 @@ variable "environment" {
   type = string
 }
 
-variable "project_name" {
-  type = string
-}
-
-variable "owner_email" {
-  type = string
-}
-
-variable "cost_centre" {
-  type = string
-}
-
-variable "data_classification" {
-  type = string
-}
 
 variable "workload_name" {
   type = string
