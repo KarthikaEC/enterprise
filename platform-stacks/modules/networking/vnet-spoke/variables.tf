8a0a86ecd58ecf212a5f77dc3d1a2fc793692cfd
KarthikaEC <karthika@Elastacloud.onmicrosoft.com>
2026-07-30 11:47:45 +0530

    removed extra variables

--- platform-stacks/modules/networking/vnet-spoke/variables.tf
+++ platform-stacks/modules/networking/vnet-spoke/variables.tf
@@ -23,32 +23,6 @@ variable "address_space" {
   type        = list(string)
 }
 
-variable "subnets" {
-  description = "Configuration for subnets within the spoke VNet."
-
-  type = map(object({
-    cidr = string
-
-    delegation = optional(object({
-      name         = string
-      service_name = string
-      actions      = optional(list(string), [])
-    }))
-
-    pe_policy = optional(string, "Enabled")
-  }))
-}
-
-variable "hub_vnet_id" {
-  description = "Resource ID of the hub VNet."
-  type        = string
-}
-
-variable "default_route_next_hop" {
-  description = "Private IP address of the firewall/NVA used as the default route next hop."
-  type        = string
-}
-
 variable "tags" {
   description = "Tags applied to networking resources."
   type        = map(string)
